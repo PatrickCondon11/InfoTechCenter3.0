57bc3ed474cc456c4a988e8c8d7278621155c695
Patrick Condon <02patrick.condon@gulllakecs.org>
2022-10-03 08:07:17 -0400

    Add Print Statements to Welcome Visitors

--- Operation Fury.py
+++ Operation Fury.py
@@ -6,3 +6,8 @@
 Our welcome screen will start our program letting drivers know
 that the info tech center OS is loading
 '''
+
+
+print("\n\nWelcome to Operation Fury Info Tech Center")
+
+print("\nOperation Fury's Operating System booting up")
