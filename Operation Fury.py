b40f98d6a33076d0e20d2297cd870714c5585113
Patrick Condon <02patrick.condon@gulllakecs.org>
2022-10-04 08:35:43 -0400

    Add Text Color to Program

--- Operation Fury.py
+++ Operation Fury.py
@@ -7,11 +7,10 @@ Our welcome screen will start our program letting drivers know
 that the info tech center OS is loading
 '''
 
-import time
+# Import Libraries Here
 from time import sleep # We imported the sleep function from the time library
 
-# Import Libraries Here
-
+print("\033[3;32;40m")
 print("\n\nWelcome to Operation Fury Info Tech Center")
 sleep(2)
-print("\nOperation Fury's Operating System booting up")
+print("\n\033[3;33;40mOperation Fury's Operating System booting up")
