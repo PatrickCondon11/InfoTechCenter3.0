a18c5c47554e8ae097133ea40b20f2e18f24a6e0
Patrick Condon <02patrick.condon@gulllakecs.org>
2022-10-05 08:39:06 -0400

    Initial Weather Commit

---
+++ Operation Fury.py
@@ -0,0 +1,10 @@
+# Weather
+# Developer: Patrick Condon
+# Version 1.0
+
+"""
+Create a function for our current weather using the
+random.choice function to determine what the weather is
+picking from a list - using if, elif & else statements
+to check the condition and print a specific list element
+"""
