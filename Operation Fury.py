72704e12ccd3f4d47aeb8552dea11789b89bde07
Patrick Condon <02patrick.condon@gulllakecs.org>
2022-10-07 08:38:42 -0400

    Added weather_alert elif statements

--- Operation Fury.py
+++ Operation Fury.py
@@ -21,12 +21,35 @@ def weather():
     return weather_condition
 
 
-weather_alert = weather()
+weather_alert1 = weather()
+
+random_int1 = random.randint(15, 30)
+random_int2 = random.randint(10, 20)
+random_int3 = random.randint(5, 25)
 
 
 def vehicle_response_system():
-    if weather_alert == "Icy":
-        print("\nVRS has changed your alarm 30 minutes earlier based on the NWS forecast of", weather_alert, "roads")
+    if weather_alert1 == "Icy":
+        print("\nVRS has changed your alarm to be", random_int1, "minutes earlier based on the NWS forecast of icy roads.")
+
+    elif weather_alert1 == "Raining" and random_int3 >= 15:
+        print("\nVRS has changed your alarm to be", random_int3, "minutes earlier based on the NWS forecast of heavy rain.")
+    elif weather_alert1 == "Raining":
+        print("\nVRS has changed your alarm to be", random_int3, "minutes earlier based on the NWS forecast of rain.")
+
+    elif weather_alert1 == "Storming":
+        print("\nVRS has changed your alarm to be", random_int2, "minutes earlier based on the NWS forecast of storms in the area.")
+
+    elif weather_alert1 == "Foggy" and random_int3 >= 15:
+        print("\nVRS has changed your alarm to be", random_int3, "minutes earlier based on the NWS forecast of heavy fog.")
+    elif weather_alert1 == "Foggy":
+        print("\nVRS has changed your alarm to be", random_int3, "minutes earlier based on the NWS forecast of fog.")
+
+    elif weather_alert1 == "Sunny":
+        print("\nBased on the NWS forecast there is clear visibility and sunny skies.")
+
+    elif weather_alert1 == "Cloudy":
+        print("\nBased on the NWS there is lowered visibility and cloudy skies.")
 
 
 vehicle_response_system()
