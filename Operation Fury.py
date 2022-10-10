d91388122282d6d4c2ef64564b6db5e19204984e
Patrick Condon <02patrick.condon@gulllakecs.org>
2022-10-10 08:23:18 -0400

    Changed sleep function values and Syntax

--- Operation Fury.py
+++ Operation Fury.py
@@ -2,13 +2,13 @@
 # Devoloper: Patrick Condon
 # Version: 1.0
 
-'''
+"""
 Our welcome screen will start our program letting drivers know
 that the info tech center OS is loading
-'''
+"""
 
 # Import Libraries Here
-from time import sleep # We imported the sleep function from the time library
+from time import sleep  # We imported the sleep function from the time library
 
 print("\033[3;32m")
 print("\n\nWelcome to Operation Fury Info Tech Center")
@@ -16,5 +16,6 @@ sleep(2)
 print("\n\033[3;34mOperation Fury's Operating System booting up\033[0m")
 for i in range(3):
     print("OS Booting Up...")
-    sleep(2)
-print("\033[3;34mOS Booted Up")
+    sleep(1)
+print("\033[3;34mOS Booted Up\033[0m")
+sleep(1.5)
