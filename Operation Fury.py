508b68636bace50c74493ac58d99a81804196a1e
Patrick Condon <02patrick.condon@gulllakecs.org>
2022-10-13 08:33:43 -0400

    WIP Street names

--- Operation Fury.py
+++ Operation Fury.py
@@ -36,29 +36,19 @@ def list_of_gas_stations():
 #th st, presidents, m #, de ave
 """
 
-"""
-def gas_station_street():
-    street_name_list = ["presidents", "10th st", "m-89"]
-    random_street_name = random.choice(street_name_list)
-    list_of_presidents = ["Washington", "Adams", "Jefferson", "Reagon", "Lincoln", "Roosevelt", "Van Buren"]
-    random_president_name = random.choice(list_of_presidents)
-    list_of_street_types = ["Ave", "St", "Blvd", "Ln"]
-    random_street_type = random.choice(list_of_street_types)
-    street_number = random.randint(1, 50)
-    michigan_highway_number = random.randint(1, 99)
-    if random_street_name == "presidents":
-        president_gas_station_road = (random_president_name, list_of_street_types)
-        return president_gas_station_road
-    if random_street_name == "10th st":
-        th_st_gas_station_road = street_number
-        return (th_st_gas_station_road and "th st")
-    if random_street_name == "m-89":
-        m_gas_station = ("m -", michigan_highway_number)
-        return ("m -" and michigan_highway_number)
-
-
-gas_station_street()
-"""
+street_name_list = ["presidents", "10th st", "m-89"]
+random_street_name = random.choice(street_name_list)
+list_of_presidents = ["Washington", "Adams", "Jefferson", "Reagon", "Lincoln", "Roosevelt", "Van Buren"]
+random_president_name = random.choice(list_of_presidents)
+list_of_street_types = ["Ave", "St", "Blvd", "Ln"]
+random_street_type = random.choice(list_of_street_types)
+street_number = random.randint(1, 50)
+michigan_highway_number = random.randint(1, 99)
+if random_street_name == "presidents":
+    president_gas_station_road = (random_president_name, list_of_street_types)
+if random_street_name == "10th st":
+     th_st_gas_station_road = street_number
+if random_street_name == "m-89":
 
 def gas_level_alert():
     if gas_level_indicator == "Empty":
@@ -73,14 +63,29 @@ def gas_level_alert():
             if calling_second_contact == ("Y" or "y"):
                 sleep(2)
                 print("Calling secondary emergency contact")
-
     elif gas_level_indicator == "Low":
         print("\033[3;31m***WARNING YOUR GAS TANK IS  EXTREMELY LOW***\n***You have 20 miles left***")
         sleep(1)
         print("\033[0mFinding closest gas station using google maps")
         sleep(2)
-        print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
-              "miles away on",)
+        if street_name_list == "president":
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+              "miles away on", random_president_name, random_street_type)
+        elif street_name_list == "m-89":
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on m -", michigan_highway_number)
+        elif street_name_list == ("10th st" and street_number <= 3):
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on", street_number, "th St")
+        elif street_name_list == ("10th st" and street_number == 3):
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on", street_number, "rd St")
+        elif street_name_list == ("10th st" and street_number == 2):
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on", street_number, "nd St")
+        elif street_name_list == ("10th st" and street_number == 1):
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on", street_number, "st St")
         if miles_to_gas_station > 20:
             sleep(1)
             print("***WARNING***\nYou will not reach the closest gas station")
@@ -100,8 +105,24 @@ def gas_level_alert():
 
     elif gas_level_indicator == "Quarter Tank":
         print("Your gas tank is at a quarter tank of gas and you have", random.randint(50, 100), "miles left.")
-        print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
-              "miles away on")
+        if street_name_list == "president":
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+              "miles away on", random_president_name, random_street_type)
+        elif street_name_list == "m-89":
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on m -", michigan_highway_number)
+        elif street_name_list == ("10th st" and street_number <= 3):
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on", street_number, "th St")
+        elif street_name_list == ("10th st" and street_number == 3):
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on", street_number, "rd St")
+        elif street_name_list == ("10th st" and street_number == 2):
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on", street_number, "nd St")
+        elif street_name_list == ("10th st" and street_number == 1):
+            print("The closest gas station is a", list_of_gas_stations(), "gas station", miles_to_gas_station,
+                  "miles away on", street_number, "st St")
 
     elif gas_level_indicator == "Half Tank":
         print("Your gas tank is at half a tank of gas and you have", random.randint(125, 175), "miles left.")
